1650565fdb8f31c4005d1b17f448b16fa38aa1c9
Son Paul <paul.angeles.chaquire@gmail.com>
2025-08-21 14:03:29 +0200

    final upd

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active")
 }))
+
+function sendMail(event){﻿
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch("https://formspree.io/f/mblkveee", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "./send_mail.html";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
